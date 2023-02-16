ad5c8e1f1ee8a2e366472d084dd80822208cf516
silashorta <92862835+silashorta@users.noreply.github.com>
2023-02-15 22:25:32 -0300

    Initial commit

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,38 @@
+import { AiFillCloseCircle } from 'react-icons/ai'
+import { FaHeart, FaRegHeart } from 'react-icons/fa'
+import './Colaborador.css'
+
+const Colaborador = ({ colaborador, corDeFundo, aoDeletar, aoFavoritar }) => {
+    function favoritar() {
+        aoFavoritar(colaborador.id)
+    }
+
+    return (
+        <div className="colaborador">
+            <AiFillCloseCircle
+                size={25}
+                className='deletar'
+                onClick={() => aoDeletar(colaborador.id)}
+            />
+
+            <div className='cabecalho'
+                style={{ backgroundColor: corDeFundo }}>
+                <img
+                    src={colaborador.imagem}
+                    alt={colaborador.nome} />
+            </div>
+
+            <div className="rodape">
+                <h4>{colaborador.nome}</h4>
+                <h5>{colaborador.cargo}</h5>
+                <div className='favoritar'>
+                    {colaborador.favorito
+                        ? <FaHeart className='favoritado' onClick={ favoritar } />
+                        : <FaRegHeart className='desfavoritado' onClick={ favoritar } />}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
